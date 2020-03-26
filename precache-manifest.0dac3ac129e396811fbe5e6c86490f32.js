422df8955776439c16785eb41bfc5438a5172e0b
Dennis <dennisweller@gmx.net>
2020-03-26 20:14:55 +0100

    Updates

---
+++ precache-manifest.0dac3ac129e396811fbe5e6c86490f32.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3ab652401d8579fcfbe0d9a0348844",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "8fe4631e21cf7832a350",
+    "url": "/todo-react/static/css/main.0b5c082c.chunk.css"
+  },
+  {
+    "revision": "1355dc723bb2dc43cd05",
+    "url": "/todo-react/static/js/2.38b91957.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.38b91957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe4631e21cf7832a350",
+    "url": "/todo-react/static/js/main.41bc84db.chunk.js"
+  },
+  {
+    "revision": "ad53fd414432102de1d6",
+    "url": "/todo-react/static/js/runtime-main.f9ba2026.js"
+  },
+  {
+    "revision": "35e77a38ca9d85c4e8974261be764879",
+    "url": "/todo-react/static/media/forkawesome-webfont.35e77a38.eot"
+  },
+  {
+    "revision": "3a9e014c2469ffa65a0ea64a24e48b44",
+    "url": "/todo-react/static/media/forkawesome-webfont.3a9e014c.woff2"
+  },
+  {
+    "revision": "44bbdbbfb5a10ba2d1ceba3c1e0fc1f5",
+    "url": "/todo-react/static/media/forkawesome-webfont.44bbdbbf.woff"
+  },
+  {
+    "revision": "78dcc9c4999659b8026a0c5213941d43",
+    "url": "/todo-react/static/media/forkawesome-webfont.78dcc9c4.svg"
+  },
+  {
+    "revision": "fc46f3dae03b2b2e1ceed60971e90c9a",
+    "url": "/todo-react/static/media/forkawesome-webfont.fc46f3da.ttf"
+  }
+]);
